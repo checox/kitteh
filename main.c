8a1654ee24735ce8f67802e458d5bc54e51da16b
Connor Poole <cpoole@usc.edu>
2014-04-08 15:13:15 -0700

    initial main

---
+++ main.c
@@ -0,0 +1,19 @@
+// main function for the project
+
+#define F_CPU 9830000
+
+#include <stdio.h>
+#include <math.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <inttypes.h>
+
+#include <util/delay.h>
+#include "./headers/lcd_files/lcd.h"
+#include <avr/eeprom>
+
+int main() {
+  //put our main code here
+}
+
