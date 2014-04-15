435da7189bec1806fbbeae8b3aa1dec2c177ecb4
Connor Poole <cpoole@usc.edu>
2014-04-15 14:58:03 -0700

    ADC init and read

--- main.c
+++ main.c
@@ -15,10 +15,35 @@
 
 void adc_init()
 {
-  //set analog to digital converter
-  ADMUX=0;
+  //set analog to digital converter for PC2 input
+  ADMUX= (1<<MUX1);
   //set analog to digital converter to be enabled with a clock prescale of /
-  ADSCRA = (1<<ADEN) | (1<<ADPS2)
+  ADSCRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADSP0);
+  //We want ours to be in free running mode so dont touch ADSCRB
+  //Fire initial conversion to get ADC warmed up
+  ADSCRA |= (1<<ADSC);
+  //Disable digital input buffer on ADC pin C2 in order to save power
+  DIDR0 = (1<<ADC2D);
+}
+
+uint16_t adc_read()
+{
+  //read from ADC, wait for conversion to finish
+  //the micro will set ADSC to 0 when conversion is finished
+  while(ADSCRA & (1<<ADSC))
+  {
+    //patience
+  }
+  //ADCL and ADCH are the registers that store the 16 bit value of the conversion
+  uint16_t result = ADCL;
+  uint16_t temp = ADCH;
+  result = result + (temp<<8);
+
+  //set ADSC bit to get the next conversion started
+  ADSCRA |= (1<<ADSC);
+
+  return result;
+}
 int main() {
   //put our main code here
 }
