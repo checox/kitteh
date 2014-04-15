b4dccba00c01be61dd13ce0f2ba982d59e11421a
Connor Poole <cpoole@usc.edu>
2014-04-15 14:28:02 -0700

    cleanup

--- main.c
+++ main.c
@@ -13,6 +13,12 @@
 #include "./headers/lcd_files/lcd.h"
 #include <avr/eeprom>
 
+void adc_init()
+{
+  //set analog to digital converter
+  ADMUX=0;
+  //set analog to digital converter to be enabled with a clock prescale of /
+  ADSCRA = (1<<ADEN) | (1<<ADPS2)
 int main() {
   //put our main code here
 }
